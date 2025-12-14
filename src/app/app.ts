1e10128ed42e1c5c3a3b9f7266b295db945e0067
Noelia Navas <noelianavasgarrido96@gmail.com>
2025-12-14 13:31:03 +0100

    -Configuración HttpClient + inStalacion Bootstrap -Creaciçon Header y Footer -ProductService + Primera llamada a la API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductService,Product } from './services/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gestion-productos');
+
+  constructor (private productService: ProductService){
+    this.productService.cargarProductos().subscribe(
+      (datos: Product[]) => {
+        console.log('Productos cargados desde la API:', datos);
+      }
+    );
+  }
 }
