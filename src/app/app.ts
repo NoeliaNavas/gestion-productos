121aecd00bcca864d9a320f55f2d1ab4a50e7d51
Noelia Navas <noelianavasgarrido96@gmail.com>
2025-12-14 15:24:35 +0100

    Formulario para añadir productos

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { CommonModule, } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { ProductService,Product } from './services/product';
 import { ProductsList } from './components/products-list/products-list';
-import { ProductCard } from './components/product-card/product-card';
+import { ProductForm } from './components/product-form/product-form';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, ProductsList, ProductCard],
+  imports: [CommonModule, ProductsList, ProductForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -19,4 +19,8 @@ export class App {
       }
     );
   }
+
+  onProductoCreado(producto: any) {
+  console.log('Producto recibido desde el formulario:', producto);
+}
 }
